42b8bcdccba7ccc55319724a8ccb52b00d94e242
Jess <74465228+euJess@users.noreply.github.com>
2022-12-29 14:11:28 -0300

    anotações adicionadas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500; // não pode ter ponto (2.500) pq conta como virgula
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 5;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 1.25; (se tentar mudar o valor dá erro por conta do "final")
     }
 }
